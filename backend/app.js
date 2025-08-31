cfc405a463052e443c9249b14e175f45c1df661f
AyushPoojariUCD <ayush.poojari@ucdconnect.ie>
2025-08-31 21:52:09 +0530

    feat: updated app.js

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,44 @@
+// Setup Express
 const express = require('express');
 
+// Setup Dotenv
+const dotenv = require('dotenv');
+dotenv.config();
+
+// CORS
+const cors = require('cors');
+
+// APP:: Express
 const app = express();
 
+// Cookie Parser
+const cookieParser = require('cookie-parser');
+
+// Connecting Database: MongoDB
+const connectToDb = require('./db/db');
+connectToDb();
+
+// Routes
+const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
+const mapsRoutes = require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
+
+// Middleware Functions
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Route Setup
 app.get('/', (req, res) => {
-    res.send("Uber Backend Running");
+    res.send('Hello World');
 });
+
+// Mounting Routes
+app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
+app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
 
 module.exports = app;
